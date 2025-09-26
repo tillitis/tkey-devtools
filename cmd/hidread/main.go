f2cb5ca4d951a8e6e849c09593a9cc9dbce81eda
Daniel Jobson <jobson@tillitis.se>
2025-09-26 14:37:09 +0200

    hidread: Scan for and open a specified VID and PID.
    Optionally retry the scan every second, and connect as soon as it is
    found.

--- cmd/hidread/main.go
+++ cmd/hidread/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"runtime/debug"
 	"strings"
+	"time"
 
 	"github.com/sstallion/go-hid"
 )
@@ -39,6 +40,9 @@ func main() {
 	size := flag.Int("s", 64, "Size of Input Reports to read")
 	versionOnly := flag.Bool("v", false, "Output version information.")
 	enumerate := flag.Bool("e", false, "Enumerate devices.")
+	vid := flag.Uint("vid", 0, "VID for device to open.")
+	pid := flag.Uint("pid", 0, "PID for device to open.")
+	continue_search := flag.Bool("c", false, "Continue scanning for device with specified VID and PID, open when it appears.")
 
 	flag.Parse()
 
@@ -51,7 +55,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	if *help || *path == "" {
+	if *help {
 		flag.Usage()
 		os.Exit(0)
 	}
@@ -66,6 +70,36 @@ func main() {
 				info.ProductStr)
 			return nil
 		})
+		os.Exit(0)
+	}
+
+	var hid_dev hid.DeviceInfo
+	if *vid != 0 && *pid != 0 {
+		for {
+			hid.Enumerate(uint16(*vid), uint16(*pid), func(info *hid.DeviceInfo) error {
+				fmt.Printf("%s: ID %04x:%04x %s %s\n",
+					info.Path,
+					info.VendorID,
+					info.ProductID,
+					info.MfrStr,
+					info.ProductStr)
+				hid_dev = *info
+
+				return nil
+			})
+
+			*path = hid_dev.Path
+
+			if *path != "" || !*continue_search {
+				break
+			}
+			time.Sleep(1 * time.Second)
+			fmt.Printf("Searching..\n")
+		}
+	}
+
+	if *path == "" {
+		fmt.Printf("Path empty\n")
 		os.Exit(0)
 	}
 
